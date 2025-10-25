00fdab39dd5a99948ef4dab95841f7a6a13a92e2
macmini.jiang <macmini@gmail.com>
2025-10-25 13:57:54 +0800

    temp

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/search/features.dart';
 import 'widgets/filter_list/features.dart';
+import 'widgets/auth/feature.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -8,7 +9,14 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [Text('Hello World'), SearchDemoPage(), FilterListDemoPage()],
+      children: [
+        Text('Hello World'),
+        SearchDemoPage(),
+        FilterListDemoPage(),
+        Divider(),
+        AuthDemoPage(),
+        SizedBox(height: 20),
+      ],
     );
   }
 }
